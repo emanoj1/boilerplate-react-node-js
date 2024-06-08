5ecb7cb5f9dd4d7c3c16aff766cb73721c68f491
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2024-06-08 14:14:51 +1000

    Stylize nav bar - move items inside

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import './App.css';
 
 const App = () => {
   return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,20 +5,23 @@ import './Header.css';
 const Header = () => {
   return (
     <header>
-      <div className="logo">
-        <Link to="/">
-          <img src="/sample-logo.png" alt="Logo" />
-        </Link>
+      <div className="header-container">
+        <div className="logo">
+          <Link to="/">
+            <img src="/sample-logo.png" alt="Logo" />
+          </Link>
+        </div>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </nav>
       </div>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
-      </nav>
     </header>
   );
 };
 
 export default Header;
+
