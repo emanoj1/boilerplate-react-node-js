ac23d9ac49cd5567071ed57ce7c6873da6544915
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2024-06-08 11:02:04 +1000

    Replace Switch with Routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -12,11 +12,11 @@ const App = () => {
       <div>
         <Header />
         <main>
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/contact" component={Contact} />
-          </Switch>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
         </main>
         <Footer />
       </div>
@@ -25,4 +25,5 @@ const App = () => {
 };
 
 export default App;
+
 
